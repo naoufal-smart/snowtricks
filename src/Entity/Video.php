0d358a2b05fbaeecc65a1a563bb0475735be9e61
Alexis Romero <romero.alexis@free.fr>
2021-03-31 16:56:16 +0200

    New Entity Video; CRUD; And Embed VideoType Inside Edit Form !!

---
+++ src/Entity/Video.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VideoRepository::class)
+ */
+class Video
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $tag;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Figure::class, inversedBy="videos")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Figure;
+
+    public function __toString(){
+        return $this->tag;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTag(): ?string
+    {
+        return $this->tag;
+    }
+
+    public function setTag(string $tag): self
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    public function getFigure(): ?Figure
+    {
+        return $this->Figure;
+    }
+
+    public function setFigure(?Figure $Figure): self
+    {
+        $this->Figure = $Figure;
+
+        return $this;
+    }
+}
